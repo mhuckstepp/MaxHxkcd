7b7824924885eb2d7b0dbecf0518d6cbd9ca75ee
Max Huckstepp <mhuckstepp@gmail.com>
2021-06-18 14:22:54 -0400

    fixes monorepo react app delivery from Heroku

--- client/src/components/Comic.js
+++ client/src/components/Comic.js
@@ -10,9 +10,7 @@ const Comic = (props) => {
         <div className=" rounded bg-white shadow m-8 p-8 flex flex-col items-center text-center max-w-screen-xl dark:bg-gray-400">
           <h2 className="text-4xl mb-8 md:text-lg m-0">{comic.title}</h2>
           <p className="text-l mb-4"># {comic.num}</p>
-
           <img className="mb-4" src={comic.img} alt={comic.alt} />
-
           <p className="text-xl m-4 md:text-xs">{comic.alt}</p>
         </div>
       </Link>
--- index.js
+++ index.js
@@ -9,35 +9,38 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.use("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 const port = process.env.PORT;
 
 app.get("/api/comic/:num", async (req, res, next) => {
-  let comicNum = Number(req.params.num)
-  let comic = await db("comics").where({num: comicNum});
+  let comicNum = Number(req.params.num);
+  let comic = await db("comics").where({ num: comicNum });
   res.status(200).json(comic);
 });
 
 app.get("/api/all", async (req, res, next) => {
-  let comics = await db("comics").orderBy('num', 'asc');
+  let comics = await db("comics").orderBy("num", "asc");
   res.json(comics);
 });
 
 app.get("/api/comments/:num", async (req, res, next) => {
-  let comicNum = Number(req.params.num)
-  let comments = await db("comments").where({num: comicNum})
+  let comicNum = Number(req.params.num);
+  let comments = await db("comments").where({ num: comicNum });
   res.json(comments);
 });
 
 app.post("/api/comic", async (req, res, next) => {
-  let comic = req.body
+  let comic = req.body;
   console.log(comic);
-  let insert = await db("comics").insert(comic).then(response => {
-    console.log(response);
-  }).catch(err => console.log(err))
+  let insert = await db("comics")
+    .insert(comic)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => console.log(err));
   res.json(comic);
 });
 
@@ -46,7 +49,7 @@ app.use("/", (req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log('listening on', port)
+  console.log("listening on", port);
 });
 
-module.exports = app
+module.exports = app;
