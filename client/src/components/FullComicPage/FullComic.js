83c625f251ce6a2a1492ef187775893955fba344
Max Huckstepp <mhuckstepp@gmail.com>
2021-06-18 14:05:39 -0400

    cleaning up error message and comment section

--- client/src/components/FullComicPage/FullComic.js
+++ client/src/components/FullComicPage/FullComic.js
@@ -3,7 +3,7 @@ import { useParams, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import {url, } from './FullComicActions'
+import { url } from "./FullComicActions";
 
 const FullComic = () => {
   let { num } = useParams();
@@ -22,7 +22,7 @@ const FullComic = () => {
 
   const handleKeyDown = (e) => {
     if (e.keyCode === 27) {
-      history.goBack()
+      history.goBack();
     }
   };
 
@@ -35,57 +35,51 @@ const FullComic = () => {
   };
 
   useEffect(() => {
-      if(comics.length) {
-      const comic = comics.filter((comic) => Number(comic.num) === num)
-        if(comic) {
-          setSelectComic(comic);
-        }
-        else {
-         axios
+    if (comics.length) {
+      const comic = comics.filter((comic) => Number(comic.num) === num);
+      if (comic) {
+        setSelectComic(comic);
+      } else {
+        axios
           .get(url(`/api/comic/${num}`))
           .then((res) => {
-          setSelectComic([res.data])
-          }).catch(err => console.log(err))
-        }
+            setSelectComic([res.data]);
+          })
+          .catch((err) => console.log(err));
       }
+    }
   }, [num, comics]); //eslint-disable-line
-  
 
-  
   useEffect(() => {
-    axios
-      .get(url(`/api/comments/${num}`))
-      .then((res) => {
-        setComments(res.data)
-      })
-  }, [num])
+    axios.get(url(`/api/comments/${num}`)).then((res) => {
+      setComments(res.data);
+    });
+  }, [num]);
 
-  
-  
-  if (!selectComic[0]){
+  if (!selectComic[0]) {
     return (
       <>
-      <div>
-      Sorry we couldn't find a comic with that number. Try again. 
-      </div>
-      <div className="flex mt-8 mb-5">
-          <button
-            className="bg-gradient-to-r mr-12 p-2 from-blue-300 rounded font-bold"
-            onClick={() => handleLast()}
-          >
-            Previous
-          </button>
-          <button
-            className="bg-gradient-to-l ml-10 p-4 pl-10 from-blue-300 rounded font-bold"
-            onClick={() => handleNext()}
-          >
-            Next
-          </button>
+        <div className="flex flex-col mt-12 items-center text-center">
+          Sorry we couldn't find a comic with that number. Try hitting previous.
+          <div className="flex mt-8 mb-5">
+            <button
+              className="bg-gradient-to-r mr-12 p-2 from-blue-300 rounded font-bold"
+              onClick={() => handleLast()}
+            >
+              Previous
+            </button>
+            <button
+              className="bg-gradient-to-l ml-10 p-4 pl-10 from-blue-300 rounded font-bold"
+              onClick={() => handleNext()}
+            >
+              Next
+            </button>
+          </div>
         </div>
-        </>
-    )
+      </>
+    );
   }
-  
+
   const { title, img, alt } = selectComic[0];
   return (
     <div>
@@ -108,11 +102,12 @@ const FullComic = () => {
         <img className=" max-h-screen" src={img} alt={alt} />
         <p className="text-2xl mt-12 w-7/12 md:mt-4 md:text-sm">{alt}</p>
         <p className="text-lg my-4 md:text-base"># {num}</p>
-        <p>Comments</p>
-        {comments.length &&
+        {comments[0] && <p>Comments</p>}
+        {comments[0] &&
           comments.map((comment) => (
             <div class="media">
-            {comment.poster} - {comment.comment} </div>
+              {comment.poster} - {comment.comment}{" "}
+            </div>
           ))}
       </div>
     </div>
