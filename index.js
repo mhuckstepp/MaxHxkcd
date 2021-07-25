2f17b6f7b6775707bf994c415b4e3c83911dceef
Max Huckstepp <mhuckstepp@gmail.com>
2021-07-25 15:35:10 -0700

    fixed package.json

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require("express")
 const cors = require("cors")
 const db = require("./db-config")
 const path = require("path")
-const dotenv = require("dotenv")
+require("dotenv").config();
 
-dotenv.config();
+
 const port = process.env.PORT;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/", function (req, res, next) {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
