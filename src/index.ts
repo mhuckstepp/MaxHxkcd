e89e37026d95f7416ed81cf3de65fbf47f66b670
Max Huckstepp <mhuckstepp@gmail.com>
2021-08-11 16:21:05 -0700

    fix package.json error

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 //eslint-disable-next-line
 // app.get("/", function (req, res, _) {
